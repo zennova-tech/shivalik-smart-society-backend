71d1911e47461c5e038f048f82a936cfd3f72413
ashish-zennova <ashishzapadiya@zennovatech.com>
2025-11-08 16:07:14 +0530

    fix: start db

--- services/society-services/src/index.js
+++ services/society-services/src/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 
 const dotenv = require("dotenv");
-const yargs = require('yargs');
+const yargs = require("yargs");
 
 const argv = yargs.argv;
-const appEnv = argv.env || process.env.NODE_ENV || 'dev';
+const appEnv = argv.env || process.env.NODE_ENV || "dev";
 
 // Set NODE_ENV to match the chosen environment
 process.env.NODE_ENV = appEnv;
@@ -18,12 +18,12 @@ console.log(`Loaded environment: ${appEnv}`);
 console.log(`MongoDB URL: ${process.env.ENTRYTRACKING_DB_URL}`);
 console.log(`Database Name: ${process.env.DB_NAME}`);
 
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 const app = express();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 const db = require("./models/index.js");
 // const { swaggerUi, swaggerDocs } = require('./src/config/swagger.js');
 const { job } = require("./console/cron");
@@ -31,30 +31,32 @@ const { job } = require("./console/cron");
 // const{syncLeadData } = require("./src/console/userRoleAssigned.js");
 const commonConfig = require("./config/common");
 // const { isSocketAuthenticated } = require('./src/middleware/authSocket.js');
-const logApiCalls = require('./middleware/loggerMiddleware.js');
-const throttleCalls = require('./middleware/throttleMiddleware.js');
-const cron = require('node-cron');
-const { setupTerritoryRabbitMQ } = require('./libs/rabbitmq.js');
+const logApiCalls = require("./middleware/loggerMiddleware.js");
+const throttleCalls = require("./middleware/throttleMiddleware.js");
+const cron = require("node-cron");
+const { setupTerritoryRabbitMQ } = require("./libs/rabbitmq.js");
 
 var whitelist = [
-    'dev-sample-services.shivalikgroup.com','35.154.180.15','35.154.180.15:3011', 'localhost:11001',
-  ];
+  "dev-sample-services.shivalikgroup.com",
+  "35.154.180.15",
+  "35.154.180.15:3011",
+  "localhost:8888",
+];
 
 var corsOption = function (req, callback) {
-    var corsOptions;
-    if (whitelist.indexOf(req.header('host')) !== -1) {
-      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  var corsOptions;
+  if (whitelist.indexOf(req.header("host")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
     //   console.log(corsOptions);
-      callback(null, corsOptions)
-    } else {
-        corsOptions = { origin: false } // disable CORS for this request
-        // console.log(corsOptions);
-        callback(new Error(`Not allowed by CORS : ${req.header('host')}`))
-
-    }
+    callback(null, corsOptions);
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
     // console.log(corsOptions);
-    // callback(null, corsOptions) // callback expects two parameters: error and options
-}
+    callback(new Error(`Not allowed by CORS : ${req.header("host")}`));
+  }
+  // console.log(corsOptions);
+  // callback(null, corsOptions) // callback expects two parameters: error and options
+};
 
 app.use(cors(corsOption));
 app.use(bodyParser.json());
@@ -63,30 +65,34 @@ app.use(fileUpload());
 app.use(throttleCalls); // Log all API calls
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'ejs');
-app.set('trust proxy', true);
+app.set("views", path.join(__dirname, "app/views"));
+app.set("view engine", "ejs");
+app.set("trust proxy", true);
 app.use(logApiCalls); // Log all API calls
 
 // CRONs
-const webRegistrationReportCron = job(cron,'30 10 * * *', "webRegistrationReportCron");
+const webRegistrationReportCron = job(
+  cron,
+  "30 10 * * *",
+  "webRegistrationReportCron"
+);
 // Import versioned router
-const v1Routes = require('./routes');
+const v1Routes = require("./routes");
 
 // Use versioned routes
-app.use('/api/v1', v1Routes);
+app.use("/api/v1", v1Routes);
 
 // Start RabbitMQ consumer
-setupTerritoryRabbitMQ().catch(err => console.error('Failed to start Territory RabbitMQ:', err));
+// setupTerritoryRabbitMQ().catch(err => console.error('Failed to start Territory RabbitMQ:', err));
 
 app.get("/", (req, res) => {
-    res.json({ message: `Welcome to FIRST application. Hello : ${envFile}` });
+  res.json({ message: `Welcome to FIRST application. Hello : ${envFile}` });
 });
 
-app.use(express.static(__dirname + '/uploads/'));
+app.use(express.static(__dirname + "/uploads/"));
 
 const PORT = process.env.PORT || 11001;
 
-const server = app.listen(PORT, '0.0.0.0', () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`http://localhost:${PORT}.`);
 });
