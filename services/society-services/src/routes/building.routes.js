af8c994b3b4ea6d74751242cdeca7b67bea8f640
harshika-zennova <harshikarabadiya@zennovatech.com>
2025-11-09 09:04:38 +0530

    fix building api

--- services/society-services/src/routes/building.routes.js
+++ services/society-services/src/routes/building.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/building.controller");
 const auth = require("../middleware/auth");
 
-router.get("/", controller.list);
+router.get("/:id", controller.list);
 // router.post("/", auth.optional, controller.create);
 router.get("/:id", controller.getById);
 router.put("/:id", controller.update);
